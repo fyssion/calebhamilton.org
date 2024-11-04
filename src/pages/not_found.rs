e941a96af3c284946085f7a2f1a90e699a4ac916
Fyssion <fyssioncodes@gmail.com>
2024-11-03 18:29:30 -0600

    Update 404 page

--- src/pages/not_found.rs
+++ src/pages/not_found.rs
@@ -4,11 +4,12 @@ use leptos_meta::*;
 #[component]
 pub fn NotFound() -> impl IntoView {
     view! {
-        <Title text="Not Found | Caleb Hamilton"/>
+        <Title text="Not Found - Caleb Hamilton"/>
         <div class="container">
             <div class="content">
                 <h1 style="text-align: center">"Aw shucks"</h1>
                 <p style="text-align: center">"This is a 404 page."</p>
+                <p style="text-align: center">"The page you're looking for was not found."</p>
             </div>
         </div>
     }
